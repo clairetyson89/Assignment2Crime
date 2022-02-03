517d62ce66b6acd6dabb33ac61d18e91907941e3
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 22:56:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ var map = L.map('map').setView([37.760109273244524, -122.4414471468871], 11.5);
 }).addTo(map);
 
 $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-	var crimeIcon = L.icon({
+	var bodyIcon = L.icon({
 		iconUrl: "https://d338t8kmirgyke.cloudfront.net/icons/icon_pngs/000/012/474/original/victim_5514707.png",
 		iconSize: [25,25]
 	});
     L.geoJson(data ,{
     pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: crimeIcon});
+	  return L.marker(latlng,{icon: bodyIcon});
     }
   } ).addTo(map);
 });
