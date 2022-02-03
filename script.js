49f3df3fac20ccdb3ffb00e8f1096e83351c01e8
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 22:51:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-var map = L.map('map').setView([37.76423006921716, -122.42960396594103], 11);
-
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
+var map = L.map('map').setView([37.760109273244524, -122.4414471468871], 11.5);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-    var ratIcon = L.icon({
-    iconUrl: 'https://cdn.icon-icons.com/icons2/3079/PNG/512/detective_crime_man_persona_vatar_icon_191245.png',
-    iconSize: [20,20]
-  }); 
-  L.geoJson(data  ,{
+$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+	var crimeIcon = L.icon({
+		iconUrl: "https://d338t8kmirgyke.cloudfront.net/icons/icon_pngs/000/012/474/original/victim_5514707.png",
+		iconSize: [25,25]
+	});
+    L.geoJson(data ,{
     pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: ratIcon});
+	  return L.marker(latlng,{icon: crimeIcon});
     }
-  }  ).addTo(map);
+  } ).addTo(map);
 });
