1efaf86fc8a1b9f30ab9ccb9f664c2b3b549c6c1
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 22:40:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var map = L.map('map').setView([37.79295721972873, -122.4197494992932], 11);
+var map = L.map('map').setView([37.76423006921716, -122.42960396594103], 11);
 
-  // load a tile layer
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
@@ -9,18 +8,14 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext
 	ext: 'png'
 }).addTo(map);
 
-  // load GeoJSON from an external file
-  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-         var ratIcon = L.icon({
-    iconUrl: 'https://icon-library.com/images/burglar-icon/burglar-icon-23.jpg',
-    iconSize: [30,30]
+ $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+    var ratIcon = L.icon({
+    iconUrl: 'https://cdn.icon-icons.com/icons2/3079/PNG/512/detective_crime_man_persona_vatar_icon_191245.png',
+    iconSize: [20,20]
   }); 
   L.geoJson(data  ,{
     pointToLayer: function(feature,latlng){
-	    return L.marker(latlng,{icon: ratIcon});
-    },
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup('<h1>Title: '+feature.properties.title+'</h1><p>Description:  '+feature.properties.description+'</p>');
+	  return L.marker(latlng,{icon: ratIcon});
     }
-  }).addTo(map);
+  }  ).addTo(map);
 });
